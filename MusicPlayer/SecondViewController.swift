f66b97f5994739498e5709d6aa8d7998c62e056d
Uysses <ucast2@gmail.com>
2014-10-24 01:41:09 -0700

    dicking with volume

--- MusicPlayer/SecondViewController.swift
+++ MusicPlayer/SecondViewController.swift
@@ -14,44 +14,21 @@ import MediaPlayer
 
 class SecondViewController: UIViewController {
     
+    @IBOutlet var volumeSlider: UISlider!
+    @IBAction func volumeSliderChanged(sender: AnyObject) {
+    
+//    playerMP.MPVolumeView
+    
+    }
 
     @IBOutlet var albumCover: UIImageView!
-       
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-//        if MPMediaQuery.songsQuery().items.count != 0 {
-//            let songs = MPMediaQuery.songsQuery().items
-//            let mediaCollection = MPMediaItemCollection(items: songs)
-//            let playerMP = MPMusicPlayerController.iPodMusicPlayer()
-//            playerMP.setQueueWithItemCollection(mediaCollection)
-////            playerMP.setQueueWithQuery(<#query: MPMediaQuery!#>)
-//            playerMP.play()
-//        }
-        
-//        if MPMediaQuery.songsQuery().items.count != 0 {
-//            var controller = MPMusicPlayerController.iPodMusicPlayer()
-//            let songs = MPMediaQuery.songsQuery().items
-//            var collection = MPMediaItemCollection(items: songs)
-//            var item: MPMediaItemCollection = currentSong
-//            controller.setQ
-//            controller.nowPlayingItem = item as? MPMediaItem
-//            controller.play()
-//        
-//        }
     }
    
     
-//    MPMusicPlayerController *controller = [MPMusicPlayerController iPodMusicPlayer];
-//    
-//    MPMediaItemCollection *collection = [[MPMediaItemCollection alloc] initWithItems:arrayOfMediaItems];
-//    MPMediaItem *item = [collection representativeItem];
-//    
-//    [controller setQueueWithItemCollection:collection];
-//    [controller setNowPlayingItem:item];
-//    
-//    [controller prepareToPlay];
-//    [controller play];
 
     
     override func didReceiveMemoryWarning() {
